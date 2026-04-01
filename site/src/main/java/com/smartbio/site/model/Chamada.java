cd75d4651eba7b33af686c9656c2ad038af03d91
sucogelad0 <joaoviniciusteo@gmail.com>
2026-03-31 23:10:04 -0300

    inicio do java

---
+++ site/src/main/java/com/smartbio/site/model/Chamada.java
@@ -0,0 +1,14 @@
+package com.smartbio.site.model;
+
+public class Chamada {
+
+    private String titulo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
